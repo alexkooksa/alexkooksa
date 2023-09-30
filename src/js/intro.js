7d09e313b06517adf8701490d02642671710bc4a
Alex Kooksa <pieceofmilk@mail.ru>
2023-10-01 02:18:21 +0300

    changed intro, footer

--- src/js/intro.js
+++ src/js/intro.js
@@ -9,43 +9,55 @@ imgLoad.on("always", function (instance, image) {
   // console.log("image is " + result + " for " + image.img.src);
 
   var introTimeline = gsap.timeline({
-    /*   onComplete: () => {
-      document.body.style.overflow = "auto";
-    }, */
+    onComplete: () => {
+      // document.body.style.overflow = "auto";
+    },
   });
 
   introTimeline.to('[data-gsap-el="greeting"]', { opacity: 1, duration: 0.75 });
   introTimeline.from('[data-gsap-el="greeting"]', { duration: 1.5, text: "" });
   introTimeline.to('[data-gsap-el="mainline-1"]', {
     opacity: 1,
-    duration: 0.5,
-    y: 0,
+    duration: 1.2,
+    fontWeight: 800
   });
   introTimeline.to('[data-gsap-el="mainline-2"]', {
     opacity: 1,
-    duration: 0.5,
-    y: 0,
-  });
+    duration: 1.5,
+    fontWeight: 800,
+  }, ">-0.75");
 
-  introTimeline.to('[data-gsap-el="secondline"]', {
+  introTimeline.to('[data-gsap-el="tagline"]', {
     opacity: 1,
     duration: 0.5,
-  });
-  introTimeline.from('[data-gsap-el="secondline"]', {
+  }, ">-0.25");
+
+  introTimeline.from('[data-gsap-el="tagline"] span', {
     duration: 1.25,
     text: "",
-  });
-  introTimeline.to('[data-gsap-el="secondline"]', {
-    duration: 0.5,
-    borderBottomWidth: 3,
-  });
-  introTimeline.to('[data-gsap-el="bg2"]', { opacity: 1, duration: 1 });
-  introTimeline.to('[data-gsap-el="bg3"]', { opacity: 1, duration: 1 });
+  }, ">-0.5");
+
+  introTimeline.to('[data-gsap-el="bg2"]', {
+    opacity: 1,
+    duration: 1,
+    onStart: () => {
+      // document.body.style.overflow = "auto";
+      document.querySelector(".homepage-intro").classList.add("no-underlay");
+    },
+  }, ">-0.5");
   introTimeline.to("header .wrap", { opacity: 1, duration: 0.5 }, ">-0.5");
-  introTimeline.to('[data-gsap-el="footnote"]', {
+  introTimeline.to('[data-gsap-el="bg3"]', { opacity: 1, duration: 1 });
+  introTimeline.to(
+    '[data-gsap-el="buttonNext"]',
+    {
+      opacity: 1,
+    },
+    ">-0.5"
+  );
+  /*   introTimeline.to('[data-gsap-el="footnote"]', {
     opacity: 1,
     duration: 0.5,
     delay: 0.5,
     y: -30,
-  });
+  }); */
 });
